592b53c5749b4a145fff6c5411cf1a7f3bd50185
Abhishek Thaker <abhishek.thaker@siftconsultancy.com>
2021-12-22 01:00:48 +0530

    Activate expense card on click and fetch real expense data from backend API

--- src/components/Profile/Expenses/ExpenseCard.js
+++ src/components/Profile/Expenses/ExpenseCard.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import './expenses.scss'
+import useComponentVisible from '../../../utils/outsideClick'
 
 const ExpenseCard = styled.div`
     position: relative;
@@ -15,17 +16,20 @@ const ExpenseCard = styled.div`
     justify-content: center;
     align-items: center;
     transition: 0.5s;
-    background: white;
     border: 2px solid #23395d;
     font-family:Futura;
+    background-color: ${props => (props.isActive ? '#aaaaaa' : 'white')};
 
     &:hover {
         background: #aaaaaa;
     }
+
 `
 
 const ExpenseCardLiteComponent = (props) => {
     
+    const [ref, isActive, setIsActive] = useComponentVisible(false)
+
     const formatDate = (datestring) => {
         const date = new Date(datestring)
         const options = { year: 'numeric', month: 'long', day: 'numeric' };
@@ -33,7 +37,11 @@ const ExpenseCardLiteComponent = (props) => {
     }
 
     return(
-        <ExpenseCard onClick={props.onClick}>
+            <ExpenseCard isActive={isActive} ref={ref} onClick={
+                () => {
+                    setIsActive(true)
+                    props.onClick()
+                }} >
             <p className="expense-title">{props.title}</p>
             <p className="expense-amount">₹ {props.amount}</p>
             <p className="expense-date">{formatDate(props.date)}</p>
